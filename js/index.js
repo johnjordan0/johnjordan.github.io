c48214a0082d1346cb1a4c9f97da0bf53faf7033
johnjordan0 <goku.luo007@gmail.com>
2025-01-31 16:37:23 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Today is a very special day and I want to tell you'.split('')
+const text = 'TodayisaveryspecialdayandIwanttotellyou'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
